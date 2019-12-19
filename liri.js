18fcb40a8f1cae9e61ede12e1f0709dd5df0eee0
Ajay Singh <ajayh2@gmail.com>
2019-12-18 20:06:31 -0500

    movieThis is working

--- liri.js
+++ liri.js
@@ -16,6 +16,9 @@ function switchFunction() {
     case "concert-this":
       concertThis();
       break;
+    case "movie-this":
+      movieThis();
+      break;
   }
 }
 function spotifyThisSong() {
@@ -37,14 +40,40 @@ function spotifyThisSong() {
   });
 }
 function concertThis() {
-    var conertURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
+  var conertURL =
+    "https://rest.bandsintown.com/artists/" +
+    userInput +
+    "/events?app_id=codingbootcamp";
 
-    axios.get(conertURL).then(
-        function (response) {
-            console.log("Artist/Band : "+userInput)
-            console.log("Concert Hall: ", response.data[0].venue.name);
-            console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
-            console.log("Time; ", moment(response.data[0].datetime).format("MM/DD/YYYY"));
-        })
+  axios.get(conertURL).then(function(response) {
+    console.log("Artist/Band : " + userInput);
+    console.log("Concert Hall: ", response.data[0].venue.name);
+    console.log(
+      "Location: " +
+        response.data[0].venue.city +
+        ", " +
+        response.data[0].venue.region
+    );
+    console.log(
+      "Time; ",
+      moment(response.data[0].datetime).format("MM/DD/YYYY")
+    );
+  });
 }
+
+function movieThis() {
+  var movieURL =
+    "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+  axios.get(movieURL).then(function(response) {
+    console.log("Title : " + response.data.Title);
+    console.log("Year : " + response.data.Year);
+    console.log("IMDB Rating : " + response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating : " + response.data.Ratings[1].Value);
+    console.log("Country Produced: " + response.data.Country);
+    console.log("Language : " + response.data.Language);
+    console.log("Plot : " + response.data.Plot);
+    console.log("Actors : " + response.data.Actors);
+  });
+}
+
 switchFunction();
