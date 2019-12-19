d5c242afa88aed48d799d5c872e632eafc6b7f2c
Ajay Singh <ajayh2@gmail.com>
2019-12-18 19:33:28 -0500

    concertTHIS is working

--- liri.js
+++ liri.js
@@ -8,6 +8,16 @@ var moment = require("moment");
 var action = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
 
+function switchFunction() {
+  switch (action) {
+    case "spotify-this-song":
+      spotifyThisSong();
+      break;
+    case "concert-this":
+      concertThis();
+      break;
+  }
+}
 function spotifyThisSong() {
   if (!userInput) {
     userInput = "The Sign";
@@ -22,15 +32,19 @@ function spotifyThisSong() {
     }
     console.log("Artist : " + data.tracks.items[0].artists[0].name);
     console.log("Name: " + data.tracks.items[0].name);
-    console.log("Spotify Link : "+ data.tracks.items[0].external_urls.spotify)
-    console.log("Album Name: " +data.tracks.items[0].album.name)
+    console.log("Spotify Link : " + data.tracks.items[0].external_conertURLs.spotify);
+    console.log("Album Name: " + data.tracks.items[0].album.name);
   });
 }
-function switchFunction() {
-  switch (action) {
-    case "spotify-this-song":
-      spotifyThisSong();
-      break;
-  }
+function concertThis() {
+    var conertURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
+
+    axios.get(conertURL).then(
+        function (response) {
+            console.log("Artist/Band : "+userInput)
+            console.log("Concert Hall: ", response.data[0].venue.name);
+            console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+            console.log("Time; ", moment(response.data[0].datetime).format("MM/DD/YYYY"));
+        })
 }
 switchFunction();
